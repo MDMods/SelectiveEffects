92686d1105656f848be3a3a6ce63450e437235d7
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-02-23 21:03:52 -0600

    Fixed bug when enabling disable all

--- Patches/MainEffectsPatch.cs
+++ Patches/MainEffectsPatch.cs
@@ -8,24 +8,17 @@ namespace SelectiveEffects.Patches
     [HarmonyPatch(typeof(Effect), nameof(Effect.CreateInstance))]
     internal class MainEffectsPatch
     {
-        internal static GameObject EmptyObject = new("EmptyObject");
-
-        [HarmonyPrefix]
-        [HarmonyPriority(Priority.VeryHigh)]
-        public static bool Prefix(ref GameObject __result)
-        {
-            if (!SettingsManager.Enabled) return true;
-
-            if (!SettingsManager.DisableAllEffects) return true;
-
-            __result = EmptyObject;
-            return false;
-        }
-
-        [HarmonyPostfix]
         public static void Postfix(Effect __instance, ref GameObject __result)
         {
             if (!SettingsManager.Enabled) return;
+
+            
+            if (SettingsManager.DisableAllEffects)
+            {
+                __result.SetActive(false);
+                return;
+            }
+            
 
             if (SettingsManager.DisableAllEffects || !EffectsDisablerManager.AnyEffect) return;
 
