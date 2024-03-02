0b860b7f98e905afc454b09591cc1cb7185c4d3b
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-03-02 15:11:21 -0600

    Updated AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 using Main = SelectiveEffects.Main;
 
-[assembly: MelonInfo(typeof(Main), "SelectiveEffects", "1.2.0", "Asgragrt")]
+[assembly: MelonInfo(typeof(Main), "SelectiveEffects", "1.2.1", "Asgragrt")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(255, 241, 196, 15)]
 
