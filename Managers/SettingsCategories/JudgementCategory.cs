e8ceb9d058371bc74b594dec8bd66ccc89d83c4a
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-01 03:01:34 -0600

    :sparkles: Added support for a custom scale value

--- Managers/SettingsCategories/JudgementCategory.cs
+++ Managers/SettingsCategories/JudgementCategory.cs
@@ -8,20 +8,23 @@ internal static partial class SettingsManager
     // Judgement Category
     //--------------------------------------------------------------------+
     internal static bool DisableJudgement => JudgementCategory._disableJudgement.Value
-                                             || (DisablePerfects && DisableGreats && DisablePass);
-
-    internal static bool MakeJudgementSmaller => JudgementCategory._makeJudgementSmaller.Value;
+                                             || (DisablePerfects && DisableGreats && DisablePass)
+                                             || ScalePercentage == 0;
+    
     internal static bool DisablePerfects => JudgementCategory._disablePerfects.Value;
     internal static bool DisableGreats => JudgementCategory._disableGreats.Value;
     internal static bool DisablePass => JudgementCategory._disablePass.Value;
+    internal static bool MakeJudgementSmaller => JudgementCategory._makeJudgementSmaller.Value;
+    internal static int ScalePercentage => JudgementCategory._scalePercentage.Value;
     
     private static class JudgementCategory
     {
         internal static MelonPreferences_Entry<bool> _disableJudgement;
-        internal static MelonPreferences_Entry<bool> _makeJudgementSmaller;
         internal static MelonPreferences_Entry<bool> _disablePerfects;
         internal static MelonPreferences_Entry<bool> _disableGreats;
         internal static MelonPreferences_Entry<bool> _disablePass;
+        internal static MelonPreferences_Entry<bool> _makeJudgementSmaller;
+        internal static MelonPreferences_Entry<int> _scalePercentage;
 
         internal static void Init()
         {
@@ -29,11 +32,19 @@ internal static partial class SettingsManager
             judgementCategory.SetFilePath(SettingsPath, true, false);
 
             _disableJudgement = judgementCategory.CreateEntry("DisableJudgement", false);
-            _makeJudgementSmaller = judgementCategory.CreateEntry("MakeJudgementSmaller", false,
-                description: "DisableJudgement takes precedence.");
             _disablePerfects = judgementCategory.CreateEntry("DisablePerfects", false);
             _disableGreats = judgementCategory.CreateEntry("DisableGreats", false);
             _disablePass = judgementCategory.CreateEntry("DisablePass", false);
+            _makeJudgementSmaller = judgementCategory.CreateEntry("MakeJudgementSmaller", false,
+                description: "DisableJudgement takes precedence.");
+            _scalePercentage = judgementCategory.CreateEntry("JudgementScalePercentage", 75,
+                description: "Range from 0-100%");
+            
+            // Verify scale is within range
+            var currentScale = ScalePercentage;
+            _scalePercentage.Value = Math.Clamp(ScalePercentage, 0, 100);
+            if (currentScale == ScalePercentage) return;
+            Melon<Main>.Logger.Warning("JudgementScalePercentage is out of bounds!");
         }
     }
 }
--- Models/JudgmentScaler.cs
+++ Models/JudgmentScaler.cs
@@ -7,20 +7,19 @@ namespace SelectiveEffects.Models;
 [RegisterTypeInIl2Cpp]
 internal class JudgmentScaler : MonoBehaviour
 {
-    //private static Vector3 _newScale = new(0.5f, 0.5f);
     public JudgmentScaler(IntPtr ptr) : base(ptr)
     {
     }
     
     public void Awake()
     {
-        if (SettingsManager.MakeJudgementSmaller) return;
+        if (SettingsManager.IsEnabled || SettingsManager.MakeJudgementSmaller) return;
 
         enabled = false;
     }
 
     public void LateUpdate()
     {
-        transform.localScale *= 0.5f;
+        transform.localScale *= (float) SettingsManager.ScalePercentage / 100;
     }
 }
