0c529110644c2af015a849802034307457668593
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-07-04 16:50:34 -0600

    ✨ Add basic support for background options

---
+++ Managers/SettingsCategories/StageCategory.cs
@@ -0,0 +1,33 @@
+﻿using MelonLoader;
+
+namespace SelectiveEffects.Managers;
+
+internal class StageCategory : Category
+{
+    private readonly MelonPreferences_Entry<bool> _disableElfin;
+    private readonly MelonPreferences_Entry<bool> _disableGirl;
+    private readonly MelonPreferences_Entry<bool> _disableStageBackground;
+    private readonly MelonPreferences_Entry<bool> _disableStageExceptFloor;
+
+    public StageCategory()
+        : base("Stage")
+    {
+        _disableStageBackground = _category.CreateEntry(
+            "DisableStageBackground",
+            false,
+            description: "Disable the stage background (Takes precedence over DisableStageExceptFloor)."
+        );
+
+        _disableStageExceptFloor = _category.CreateEntry(
+            "DisableStageExceptFloor",
+            false,
+            description: "Disable the stage background except the floor."
+        );
+    }
+
+    internal bool DisableElfin => _disableElfin.Value;
+    internal bool DisableGirl => _disableGirl.Value;
+    internal bool DisableStage => DisableStageBackGround || DisableStageExceptFloor;
+    internal bool DisableStageBackGround => _disableStageBackground.Value;
+    internal bool DisableStageExceptFloor => _disableStageExceptFloor.Value;
+}
---
+++ Patches/StagePatch.cs
@@ -0,0 +1,96 @@
+using HarmonyLib;
+using Il2CppAssets.Scripts.UI.Panels;
+using Il2CppGameLogic;
+using SelectiveEffects.Managers;
+using UnityEngine;
+
+namespace SelectiveEffects.Patches;
+
+[HarmonyPatch(typeof(PnlBattle), nameof(PnlBattle.GameStart))]
+internal static class StagePatch
+{
+    public static bool InvariantContains(this string s1, string s2) =>
+        s1?.IndexOf(s2, StringComparison.InvariantCultureIgnoreCase) != -1;
+
+    internal static void Postfix()
+    {
+        if (!SettingsManager.Get<MainCategory>().IsEnabled)
+            return;
+
+        var stageCategory = SettingsManager.Get<StageCategory>();
+        if (!stageCategory.DisableStage)
+            return;
+
+        var musicScene = GameGlobal.gGameMusicScene;
+        List<GameObject> scenes = [];
+        if (musicScene?.scenes != null)
+        {
+            foreach (var scene in musicScene.scenes)
+            {
+                scenes.Add(scene);
+            }
+        }
+        else
+        {
+            scenes.Add(musicScene.scene);
+        }
+
+        if (stageCategory.DisableStageBackGround)
+        {
+            foreach (var scene in scenes)
+            {
+                var activeStatus = scene.active;
+
+                scene.SetActiveRecursively(false);
+
+                scene.active = activeStatus;
+            }
+            return;
+        }
+
+        if (stageCategory.DisableStageExceptFloor)
+        {
+            foreach (var scene in scenes)
+            {
+                var activeStatus = scene.active;
+
+                RecursiveFloor(scene);
+
+                scene.active = activeStatus;
+            }
+
+            return;
+        }
+    }
+
+    private static bool RecursiveFloor(GameObject parent) => RecursiveFloor(parent.transform);
+
+    private static bool RecursiveFloor(Transform parent)
+    {
+        var name = parent.name;
+
+        if (name.Contains("floor", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return true;
+        }
+
+        var childCount = parent.childCount;
+        if (childCount > 0)
+        {
+            var isFloorParent = false;
+            List<Transform> children = [];
+            for (var i = 0; i < childCount; i++)
+            {
+                isFloorParent |= RecursiveFloor(parent.GetChild(i));
+            }
+
+            parent.gameObject.active = isFloorParent;
+            return isFloorParent;
+        }
+
+        var isFloor = name.InvariantContains("Floor") || name.InvariantContains("Road");
+
+        parent.gameObject.active = isFloor;
+        return isFloor;
+    }
+}
