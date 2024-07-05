b32621590d4e95ed496e4478b738e9666518c54d
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-07-04 19:43:10 -0600

    ✨ Disable elfin

--- Patches/GameScenePatch.cs
+++ Patches/GameScenePatch.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using Il2Cpp;
 using Il2CppAssets.Scripts.GameCore.GameObjectLogics.ExtraControl;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using Il2CppSpine.Unity;
 using MelonLoader;
 using SelectiveEffects.Managers;
@@ -10,11 +11,37 @@ namespace SelectiveEffects;
 [HarmonyPatch]
 internal static class GameScenePatch
 {
+    [HarmonyPatch(
+        typeof(ElfinCreate),
+        nameof(ElfinCreate.OnBattleStart),
+        typeof(Il2CppSystem.Object),
+        typeof(Il2CppSystem.Object),
+        typeof(Il2CppReferenceArray<Il2CppSystem.Object>)
+    )]
+    [HarmonyPostfix]
+    internal static void ElfinPostfix(ElfinCreate __instance)
+    {
+        if (
+            !SettingsManager.Get<MainCategory>().IsEnabled
+            || !SettingsManager.Get<GameSceneCategory>().DisableElfin
+        )
+            return;
+
+        var skMechanism = __instance.m_SkeletonMecanim;
+        if (!skMechanism)
+            return;
+
+        skMechanism.skeleton.a = 0;
+    }
+
     [HarmonyPatch(typeof(GirlActionController), nameof(GirlActionController.Init))]
     [HarmonyPostfix]
     internal static void GirlInitPostfix(GirlActionController __instance)
     {
-        if (!SettingsManager.Get<GameSceneCategory>().DisableGirl)
+        if (
+            !SettingsManager.Get<MainCategory>().IsEnabled
+            || !SettingsManager.Get<GameSceneCategory>().DisableGirl
+        )
             return;
         var girlSk = __instance.go?.GetComponent<SkeletonAnimation>();
         if (girlSk)
@@ -33,8 +60,13 @@ internal static class GameScenePatch
     [HarmonyPostfix]
     internal static void GirlShadowPostfix(RoleBattleSubControl __instance)
     {
-        if (!SettingsManager.Get<GameSceneCategory>().DisableGirl)
+        // ! Fix this method
+        if (
+            !SettingsManager.Get<MainCategory>().IsEnabled
+            || !SettingsManager.Get<GameSceneCategory>().DisableGirl
+        )
             return;
+
         if (!__instance.name.InvariantContains("shadow"))
             return;
 
