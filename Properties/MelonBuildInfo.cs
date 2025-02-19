01926d6cd8bb61f239bfe77b83bac021efea6c9a
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2025-02-18 21:17:55 -0600

    🔖 Version bump

--- Properties/MelonBuildInfo.cs
+++ Properties/MelonBuildInfo.cs
@@ -6,5 +6,5 @@ internal static class MelonBuildInfo
 
     internal const string ModName = "SelectiveEffects";
 
-    internal const string ModVersion = "1.4.0";
+    internal const string ModVersion = "1.4.1";
 }
